378a8c54a7c4349dd38dfd0338dcee11be4c3dac
Connor Sullivan <cksullvn@gmail.com>
2020-03-04 09:27:13 -0600

    added in class notes for Animals repo

---
+++ Animals/Reptiles/Alligator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals.Reptiles
+{
+    internal class Alligator
+    {
+        // internal: I am only accessible inside this project
+
+        public bool IsAquatic { get; set; }
+        //underscore typically indicates a field which is intended to be private, as opposed to a property
+        private int _stepGoal;
+        private int _stepCount;
+
+        public Alligator(int stepGoal)
+        {
+            IsAquatic = true;
+            _stepGoal = stepGoal;
+        }
+
+        public void Grunt()
+        {
+            if (IsAquatic)
+            {
+                Console.WriteLine("Bubble Bubble");
+            }
+            else
+            {
+                Console.WriteLine("GRRRRR!");
+            }
+        }
+
+        public void Run(int steps)
+        {
+            _stepCount += steps;
+
+            Console.WriteLine($"You ran { steps} steps");
+
+            if (steps >= _stepGoal)
+            {
+                Console.WriteLine("Congratulations, you achieved your goal!!!!!!!!!!");
+            }
+        }
+    }
+}
